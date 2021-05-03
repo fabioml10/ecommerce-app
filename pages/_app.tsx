510f3e0f5d119ed28701a85a381e19e18076b93d
Fabio Muller <fabioml10@yahoo.com.br>
2021-05-03 15:15:53 -0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import Head from 'next/head'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../styles/globals.css'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>OneBitGames</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
