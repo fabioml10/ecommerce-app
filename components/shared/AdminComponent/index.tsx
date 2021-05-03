0ec01e6fded7c2d1f65914d5b39b38d58db355de
Fabio Muller <fabioml10@yahoo.com.br>
2021-05-03 18:39:14 -0300

    Create lateral menu

--- components/shared/AdminComponent/index.tsx
+++ components/shared/AdminComponent/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import AdminFooter from '../Footer/AdminFooter';
 import AdminHeader from '../Header/AdminHeader';
+import LateralMenu from '../LateralMenu';
 
 const AdminComponent: React.FC = ({ children }) => {
   return (
     <Row className="mr-lg-4">
       <Col lg={3}>
-        Menu Lateral
+        <LateralMenu />
       </Col>
       <Col lg={9}>
         <div className="d-flex flex-column sticky-footer-wrapper container">
