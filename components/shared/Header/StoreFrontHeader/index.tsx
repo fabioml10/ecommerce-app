be6745dfb98d0e580a06f5ca9b6f491a864a94be
Fabio Muller <fabioml10@yahoo.com.br>
2021-05-10 20:27:13 -0300

    Create search on home

--- components/shared/Header/StoreFrontHeader/index.tsx
+++ components/shared/Header/StoreFrontHeader/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Col, InputGroup, FormControl } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faShoppingCart, faUserCircle } from '@fortawesome/free-solid-svg-icons'
-
-import styles from './styles.module.css'
-
+import ProductSearchService from '../../../../util/ProductSearchService';
 import Logo from '../../Logo'
 import Link from 'next/link'
 
+import { useRouter } from 'next/router';
+import styles from './styles.module.css'
+
+
 const StoreFrontHeader: React.FC = () => {
+  const [search, setSearch] = useState('');
+
+  const router = useRouter();
+
+  const handleSearch = (): void => {
+    router.push(`
+      /search${ProductSearchService.execute({ search })}
+    `);
+  }
+
   return (
     <Row className={styles.background}>
       <Col md={6} className="mt-2">
@@ -17,22 +29,36 @@ const StoreFrontHeader: React.FC = () => {
       <Col md={6} className="mt-2 text-center">
         <Row>
           <Col md={6} className="mb-4 mb-md-0">
-            <InputGroup>
-              <FormControl placeholder="Pesquisar produto"></FormControl>
+            <InputGroup className={`${router.pathname === '/search' ? styles.hidden : ''}`}>
+              <FormControl
+                placeholder="Pesquisar produto"
+                value={search}
+                onChange={
+                  (evt: React.ChangeEvent<HTMLInputElement>) =>
+                    setSearch(evt.target.value)
+                }
+                onKeyUp={
+                  (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (evt.key.toLowerCase() === 'enter') {
+                      handleSearch();
+                    }
+                  }
+                }
+              />
             </InputGroup>
           </Col>
           <Col md={6}>
             <Row>
-              <Col md={4} xs={4}>
-                <FontAwesomeIcon icon={faSearch} color="var(--color-gray-ligth"></FontAwesomeIcon>
+              <Col>
+                <FontAwesomeIcon icon={faSearch} color="var(--color-gray-ligth)"></FontAwesomeIcon>
               </Col>
-              <Col md={4} xs={4}>
-                <FontAwesomeIcon icon={faShoppingCart} color="var(--color-gray-ligth"></FontAwesomeIcon>
+              <Col>
+                <FontAwesomeIcon icon={faShoppingCart} color="var(--color-gray-ligth)"></FontAwesomeIcon>
               </Col>
-              <Col md={4} xs={4}>
+              <Col>
                 <Link href="auth/login">
                   <a>
-                    <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-ligth"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-ligth)"></FontAwesomeIcon>
                   </a>
                 </Link>
               </Col>
